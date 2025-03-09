5de3a816d4f2dd463b1ed989a911d3771649402c
Hyperboid <23138363+Hyperboid@users.noreply.github.com>
2025-03-09 00:44:09 -0500

    Maybe make actions real

--- mkenv.sh
+++ mkenv.sh
@@ -4,5 +4,4 @@ set -e
 rm -rf venv
 python3 -m venv venv
 source venv/bin/activate
-pip3 install pe_tools
-pip3 install json5
+python3 pre.py
---
+++ pre.py
@@ -0,0 +1,4 @@
+import subprocess
+
+subprocess.run(["pip3", "install", "pe_tools"])
+subprocess.run(["pip3", "install", "json5"])
